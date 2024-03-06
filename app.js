f725fe63390d7b9c8329a466d20d6e22fd77403f
Taha Khan <ktaha7577@gmail.com>
2024-03-07 03:22:14 +0500

    conditions

---
+++ app.js
@@ -0,0 +1,13 @@
+// === equal to 
+// > greter than ziada
+// < less tahn kam
+
+
+
+
+var userAge = +prompt('Enter your age')
+if (userAge === 20) {
+    console.log('You are a teenager')
+}
+else {console.log ('You are not a teenager')
+}
